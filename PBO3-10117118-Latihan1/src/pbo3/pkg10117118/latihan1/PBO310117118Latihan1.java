fb76630b45d0e10727b70decf6c7a639a9aa98d4
AlfanWahyudi <36851954+AlfanWahyudi@users.noreply.github.com>
2018-09-23 12:56:00 +0700

    Alfan Wahyudi

---
+++ PBO3-10117118-Latihan1/src/pbo3/pkg10117118/latihan1/PBO310117118Latihan1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117118.latihan1;
+
+/**
+ *
+ * @author xider
+ * NAMA  : Alfan Wahyudi
+ * KELAS : PBO3
+ * NIM   : 10117118
+ * Deskripsi Program : Perintah Menampilkan Value Ke Layar
+ */
+public class PBO310117118Latihan1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         System.out.println("BIODATA ROCKSTAR" + "\n" + "============="
+                + "====================");
+        System.out.println("NAMA\t\t: Alfan Wahyudi");
+        System.out.println("ALAMAT\t\t: Sekeloa");
+        System.out.println("TEMPAT LAHIR\t: Sukabumi");
+        System.out.println("TANGGAL LAHIR\t: 22 Mei 1999");
+        System.out.println("=================================");
+        System.out.println("HOBI\t\t: Memancing");
+        System.out.println("MAKES\t\t: Sate");
+        System.out.println("MIKES\t\t: Dawet"); 
+    }
+    
+}
